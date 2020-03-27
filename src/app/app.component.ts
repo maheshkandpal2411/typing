e173d3225870fa807c0c02e55b2c216c54f2785e
Mishty Kandpal <mkandpal19@stlouishighschool.ie>
2020-03-27 21:55:02 +0000

    Add typing code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string) {
+    console.log(value);
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string): string {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return (randomLetter === enteredLetter) ? 'correct' : 'incorrect';
+  }
 }
